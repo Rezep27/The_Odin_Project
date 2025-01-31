e91e7d1d5b45d1c1492bfc6f1d4502c3e9f5053b
Rezep <jairospm2002@gmail.com>
2025-01-31 06:54:55 -0500

    Tested event object \'e\'

--- Test/main.js
+++ Test/main.js
@@ -1,3 +1,7 @@
 let btn = document.querySelector('#myButton');
 
 btn.addEventListener("click", () => alert("I am so sexy"));
+
+btn.addEventListener("click", function(e) {console.log(e.target);});
+
+btn.addEventListener("click", function(e) {e.target.style.background = "blue";});
