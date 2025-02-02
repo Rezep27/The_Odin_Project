f43ec28bb644be8d8be67300bdfda6d8a447aa12
Rezep <jairospm2002@gmail.com>
2025-02-02 09:38:10 -0500

    Finished DOM basic manipulation exercise

--- Odin Exercises/dom-manipulation/Basic/main.js
+++ Odin Exercises/dom-manipulation/Basic/main.js
@@ -16,3 +16,23 @@ const text = document.createTextNode(
 
 const linkPara = document.querySelector("p");
 linkPara.appendChild(text);
+
+//Remove from if we have both node and parent reference
+
+sect.removeChild(linkPara);
+
+// Remove if we have the node reference (only on modern browsers)
+// linkPara.remove()
+
+// On older browsers if we only have node reference
+// linkPara.parentNode.removeChild(linkPara);
+
+// Modify inline CSS
+// para.style.color = "white";
+// para.style.backgroundColor = "black";
+// para.style.padding = "10px";
+// para.style.width = "250px";
+// para.style.textAlign = "center";
+
+para.setAttribute("class", "highlight");
+
